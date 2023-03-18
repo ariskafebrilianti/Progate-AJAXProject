9fe0cb08f9b3f4476b405b9e0f50468254cc77b8
ariskafebrilianti <bncliany@gmail.com>
2023-03-18 16:05:01 +0700

    update2

--- ajax.js
+++ ajax.js
@@ -1,13 +1,18 @@
+async function getData(callback) {
+    const data = await fetch("https://api.covid19api.com/summary")
+    const res = await data.json()
+    
+    callback(res)
+}
 
-fetch('https://api.covid19api.com/summary')
-  .then(response => response.json())
-  .then(data => {
-    const globalData = data.Global;
-    const totalCases = globalData.TotalConfirmed;
-    const totalDeaths = globalData.TotalDeaths;
-    const totalRecovered = globalData.TotalRecovered;
 
-    // masukan kode untuk menampilkan pada HTML
-		// ...
-  })
-  .catch(error => console.error(error));
+function renderData(data) {
+    console.log(data)
+    const userElement = document.getElementById("users")
+    const globalData =data.Global
+    const totalCases = globalData.TotalConfirmed
+    const totalDeaths = globalData.totalDeaths
+    const totalRecovered = globalData.totalRecovered
+}
+
+getData(renderData)
