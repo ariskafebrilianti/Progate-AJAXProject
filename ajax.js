c8cd896e9d72e15ca81180d44691ba88fc03e7ba
ariskafebrilianti <bncliany@gmail.com>
2023-03-20 17:17:54 +0700

    final bgt

--- ajax.js
+++ ajax.js
@@ -1,40 +1,40 @@
 fetch("https://api.covid19api.com/summary")
   .then((response) => response.json())
   .then((data) => {
-    const globalData = data.Global;
-    const totalCases = globalData.TotalConfirmed;
-    const totalDeaths = globalData.TotalDeaths;
-    const totalRecovered = globalData.TotalRecovered;
+    const globalData = data.Global
+    const totalCases = globalData.TotalConfirmed
+    const totalDeaths = globalData.TotalDeaths
+    const totalRecovered = globalData.TotalRecovered
 
-    const countries = data.Countries;
+    const countries = data.Countries
 
-    console.log(countries.length);
+    console.log(countries.length)
     for (let i = 0; i < countries.length; i++) {
-      const div = document.createElement("div");
-      div.id = "country";
-      document.getElementById("countries").appendChild(div);
+      const div = document.createElement("div")
+      div.id = "country"
+      document.getElementById("countries").appendChild(div)
 
     }
     for (let i = 0; i < countries.length; i++) {
-      const ele = document.createElement("h3");
-      ele.innerHTML = countries[i].Slug;
+      const ele = document.createElement("h3")
+      ele.innerHTML = countries[i].Slug
 
-      const countryCases = document.createElement("h4");
-      countryCases.innerText = "Cases: ";
-      countryCases.innerText += countries[i].TotalConfirmed;
+      const countryDeaths = document.createElement("h4")
+      countryDeaths.innerText = "Deaths "
+      countryDeaths.innerText += countries[i].TotalDeaths
 
-      const countryDeaths = document.createElement("h4");
-      countryDeaths.innerText = "Deaths: ";
-      countryDeaths.innerText += countries[i].TotalDeaths;
+      const countryCases = document.createElement("h4")
+      countryCases.innerText = "Cases "
+      countryCases.innerText += countries[i].TotalConfirmed
 
-      document.querySelectorAll("#country")[i].appendChild(ele);
-      document.querySelectorAll("#country")[i].appendChild(countryCases);
-      document.querySelectorAll("#country")[i].appendChild(countryDeaths);
+      document.querySelectorAll("#country")[i].appendChild(ele)
+      document.querySelectorAll("#country")[i].appendChild(countryCases)
+      document.querySelectorAll("#country")[i].appendChild(countryDeaths)
     }
 
-    document.getElementById("totalCases").innerText += " " + totalCases;
-    document.getElementById("totalDeaths").innerText += " " + totalDeaths;
-    document.getElementById("totalRecovered").innerText += " " + (totalCases - totalDeaths);
+    document.getElementById("totalCases").innerText += " " + totalCases
+    document.getElementById("totalDeaths").innerText += " " + totalDeaths
+    document.getElementById("totalRecovered").innerText += " " + (totalCases - totalDeaths)
   })
 
-  .catch((error) => console.error(error));
+  .catch((error) => console.error(error))
